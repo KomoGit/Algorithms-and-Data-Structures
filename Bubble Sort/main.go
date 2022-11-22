3d018674bb5f26bed46f0517994082abb89ebc9f
James <entreur@gmail.com>
2022-11-22 14:54:45 +0400

    https://www.youtube.com/watch?v=98yDJ5vao5Q

--- Bubble Sort/main.go
+++ Bubble Sort/main.go
@@ -5,7 +5,6 @@ import "log"
 var numbers = []int{5, 6, 2, 3, 1, 4, 7, 9, 8, 10}
 
 func main() {
-	//i := 0
 	for j := 0; j < len(numbers); j++ {
 		for i := 0; i < len(numbers)-1; i++ {
 			if numbers[i] > numbers[i+1] {
